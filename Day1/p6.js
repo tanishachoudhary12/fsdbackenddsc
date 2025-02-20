d47b7bf30150c36b12b8f6b387220bd3542d6d93
tanishachoudhary12 <tanishachoudhary1234567890@gmail.com>
2025-02-20 14:52:30 +0530

    Day1 fs promises

--- Day1/p6.js
+++ Day1/p6.js
@@ -1,11 +1,11 @@
 const fs=require('fs');
 const write=(data)=>{
-    fs.writeFile("./data.txt",data,(err)=>{
+    fs.writeFile("./mydir/data.txt",data,(err)=>{
         if(err)
             console.log("Error writing file",err);
         else
         console.log("File written successfully");
     });
 }
-write("I am new data");
+write("Tanisha");
    
---
+++ Day1/p9.js
@@ -0,0 +1,6 @@
+const fs=require('fs')
+fs.mkdir('mydir',(err)=>{
+if(err) throw err;
+else
+console.log("Directory created")
+})
